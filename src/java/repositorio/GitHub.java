d01ba5c212663d594d307018b13ec7f23747f39d
LUIS <LUIS@DESKTOP-2OPHCBU>
2024-10-21 20:30:22 -0500

    Creando mi primer repositorio web

---
+++ src/java/repositorio/GitHub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio;
+
+/**
+ *
+ * @author LUIS
+ */
+public class GitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primer repositorio");
+    }
+    
+}
